b7e189ac6c20943a0b92c1bd17b86a56483c7bf1
HyeSoo Park <hyesoo.park@wolt.com>
2020-07-11 10:16:34 +0300

    Add start timer

---
+++ timer.js
@@ -0,0 +1,35 @@
+const status = document.querySelector("#timer-message");
+const timerDisplay = document.querySelector("#timer-display");
+
+const startButton = document.querySelector("#start-button");
+const pauseButton = document.querySelector("#pause-button");
+const stopButton = document.querySelector("#stop-button");
+
+let timerSeconds = 1500;
+let currentSessionTime = 1500;
+let breakSessionTime = 300;
+let timerInterval = null;
+
+const timer = () => {
+  currentSessionTime--;
+  if (currentSessionTime < 0) {
+    clearInterval(timerInterval);
+    currentSessionTime = 0;
+    status.textContent = "Take a break";
+  }
+  displayTimeLeft(currentSessionTime);
+};
+
+startButton.addEventListener("click", () => {
+  currentSessionTime = timerSeconds;
+  timerInterval = setInterval(timer, 1000);
+  status.textContent = "Keep Working";
+});
+
+const displayTimeLeft = (seconds) => {
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds % 60;
+  timerDisplay.textContent = `${minutes}:${
+    remainderSeconds < 10 ? "0" : ""
+  }${remainderSeconds}`;
+};
